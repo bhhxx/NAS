6f386a6828d848376ca79e8223106d4f5670ab2e
root <root@YJZQ.>
2025-04-09 21:03:18 +0800

    config modify

--- server/Config.hpp
+++ server/Config.hpp
@@ -1,108 +1,104 @@
 #pragma once
-#include "Util.hpp"
 #include <memory>
 #include <mutex>
-// 该类用于读取配置文件信息
+#include "Util.hpp"
+
+
 namespace storage
 {
-    // 懒汉模式
     const char *Config_File = "Storage.conf";
+
+    //
+    // 配置文件读取类
+    // 
     class Config
     {
     private:
         int server_port_;
-        std::string server_ip;
-        std::string download_prefix_; // URL路径前缀
-        std::string deep_storage_dir_;     // 深度存储文件的存储路径
-        std::string low_storage_dir_;     // 浅度存储文件的存储路径
-        std::string storage_info_;     // 已存储文件的信息
-        int bundle_format_;//深度存储的文件后缀，由选择的压缩格式确定
-    private:
-        static std::mutex _mutex;
-        static Config *_instance;
+        std::string server_ip_;
+        std::string download_prefix_;
+        std::string deep_storage_dir_;
+        std::string low_storage_dir_;
+        int bundle_format_;
+        std::string storage_info_;
+    public:
+        static std::mutex _mutex;  // 声明（告诉编译器存在这个静态成员）
+        static Config *_instance; // 声明 单例模式
         Config()
         {
-            if (ReadConfig() == false)
+            if (!ReadConfig()) // 读取配置文件
             {
-                mylog::GetLogger("asynclogger")->Fatal("ReadConfig failed");
-                return;
             }
-            mylog::GetLogger("asynclogger")->Info("ReadConfig complicate");
         }
-
     public:
-        // 读取配置文件信息
+        //
+        // 读取配置文件 
+        //
         bool ReadConfig()
         {
-            mylog::GetLogger("asynclogger")->Info("ReadConfig start");
-
-            storage::FileUtil fu(Config_File);
-            std::string content;
-            if (!fu.GetContent(&content))
+            
+            Json::Value config_json;
+            storage::FileUtil config_file(Config_File);
+            std::string config_str;
+            if (!config_file.GetContent(&config_str))
             {
                 return false;
             }
+            storage::JSON_util::UnSerialize(config_str, config_json);
 
-            Json::Value root;
-            JsonUtil::UnSerialize(content, &root); // 反序列化，把内容转成jaon value格式
-
-            // 要记得转换的时候用上asint，asstring这种函数，json的数据类型是Value。
-            server_port_ = root["server_port"].asInt();
-            server_ip = root["server_ip"].asString();
-            download_prefix_ = root["download_prefix"].asString();
-            storage_info_ = root["storage_info"].asString();
-            deep_storage_dir_ = root["deep_storage_dir"].asString();
-            low_storage_dir_ = root["low_storage_dir"].asString();
-            bundle_format_ = root["bundle_format"].asInt();
-            
+            server_port_ = config_json["server_port"].asInt();
+            server_ip_ = config_json["server_ip"].asString();
+            download_prefix_ = config_json["download_prefix"].asString();
+            deep_storage_dir_ = config_json["deep_storage_dir"].asString();
+            low_storage_dir_ = config_json["low_storage_dir"].asString();
+            bundle_format_ = config_json["bundle_format"].asInt();
+            storage_info_ = config_json["storage_info"].asString();
             return true;
-        }
-        int GetServerPort()
-        {
-            return server_port_;
-        }
-        std::string GetServerIp()
-        {
-            return server_ip;
-        }
-        std::string GetDownloadPrefix()
-        {
-            return download_prefix_;
-        }
-        int GetBundleFormat()
-        {
-            return bundle_format_;
-        }
-        std::string GetDeepStorageDir()
-        {
-            return deep_storage_dir_;
-        }
-        std::string GetLowStorageDir()
-        {
-            return low_storage_dir_;
-        }
-        std::string GetStorageInfoFile()
-        {
-            return storage_info_;
         }
 
     public:
-        // 获取单例类对象
-        static Config *GetInstance()
-        {
-            if (_instance == nullptr)
-            {
+        //
+        // 获取配置文件内容
+        //
+        int GetServerPort() {
+            return server_port_;
+        }
+        std::string GetServerIp() {
+            return server_ip_;
+        }
+        std::string GetDownloadPrefix() {
+            return download_prefix_;
+        }
+        int GetBundleFormat() {
+            return bundle_format_;
+        }
+        std::string GetDeepStorageDir() {
+            return deep_storage_dir_;
+        }
+        std::string GetLowStorageDir() {
+            return low_storage_dir_;
+        }
+        std::string GetStorageInfoFile() {
+            return storage_info_;
+        }
+
+        //
+        // 单例模式
+        //
+        static Config *GetInstance() {
+            if (_instance == nullptr) {
                 _mutex.lock();
-                if (_instance == nullptr)
-                {
+                if (_instance == nullptr) {
                     _instance = new Config();
                 }
                 _mutex.unlock();
             }
             return _instance;
         }
+
     };
     // 静态成员初始化，先写类型再写类域
-    std::mutex Config::_mutex;
+    // 在 C++ 中，静态成员变量 属于类本身（而非类的某个对象），因此需要在类外进行定义和初始化。
+    std::mutex Config::_mutex; // 定义（告诉编译器存在这个静态成员）
     Config *Config::_instance = nullptr;
 }
